e1a566b3c07ea92b2c5d65172b1940b59eb0fe6d
Jimy Mora <jimymora@gmail.com>
2024-01-26 10:45:27 -0500

    datetime, fechas

---
+++ año_mes_dia.py
@@ -0,0 +1,4 @@
+from datetime import *
+
+mi_fecha = (2024, 1, 26)  # Año, mes, día
+print("Año:", mi_fecha[0], "Mes:", mi_fecha[1], "Día:", mi_fecha[2])
---
+++ ejemplo.py
@@ -0,0 +1,12 @@
+from datetime import *
+
+mi_hora = time(17,56)
+print(mi_hora.hour)
+
+
+mi_minuto = time(17, 35)
+print(mi_minuto.minute)
+
+mi_fecha = (2024, 1, 26)  # Año, mes, día
+print(mi_fecha[0])  # Imprime el año
+
